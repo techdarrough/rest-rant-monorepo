ad4187a01d46ef44785a150f5e542bb218f36c56
Preston Darrough <techdarrough@gmail.com>
2022-06-12 08:51:21 -0700

    assignment complete

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,10 +1,17 @@
 const router = require('express').Router()
 const db = require("../models")
+const bcrypt = require('bcrpyt')
 
 const { User } = db
 
 router.post('/', async (req, res) => {
-    const user = await User.create(req.body)
+    let ( password, ...rest ) = req.body
+    const user = await User.create({
+        const user = await User.create({
+            ...rest,
+            passwordDigest: await bcrypt.has(password, 10)
+        })
+    })
     res.json(user)
 })
 
