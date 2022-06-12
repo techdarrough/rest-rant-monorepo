58f68fbb52ebf2ed47c1f3f6ac0fbe8ff08423b9
Preston Darrough <techdarrough@gmail.com>
2022-06-12 11:01:26 -0700

    sessions testing assignment complete

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -17,10 +17,38 @@ router.post('/', async (req, res) => {
             message: `Could not find a user with the provided username and password` 
         })
     } else {
+        req.session.userId = user.userId
         res.json({ user })
     }
 })
 
-
+// ___
+// router.get('/profile', async (req, res) => {
+//     try {
+//         let user = await User.findOne({
+//             where: {
+//                 userId: __
+//             }
+//         })
+//         res.json(user)
+//     } catch {
+//         res.json(null)
+//     }
+// })
+// router.get('/profile', async (req, res) => {
+//     res.json(req.currentUser)
+// })
+router.get('/profile', async (req, res) => {
+    try {
+        let user = await User.findOne({
+            where: {
+                userId: req.session.userId
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
+    }
+})
 
 module.exports = router
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,16 +1,14 @@
 const router = require('express').Router()
 const db = require("../models")
-const bcrypt = require('bcrpyt')
+const bcrypt = require('bcrypt') 
 
 const { User } = db
 
 router.post('/', async (req, res) => {
-    let ( password, ...rest ) = req.body
-    const user = await User.create({
-        const user = await User.create({
-            ...rest,
-            passwordDigest: await bcrypt.has(password, 10)
-        })
+    let { password, ...rest } = req.body;
+    const user = await User.create({ 
+        ...rest, 
+        passwordDigest: await bcrypt.hash(password, 10)
     })
     res.json(user)
 })
